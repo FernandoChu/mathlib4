e33e495e8f8cb0dcfbac453bf7d7811086506582
FernandoChu <17756312+FernandoChu@users.noreply.github.com>
2025-07-01 07:41:47 +0200

    Go back to products

--- Mathlib/CategoryTheory/DinatTrans.lean
+++ Mathlib/CategoryTheory/DinatTrans.lean
@@ -26,19 +26,18 @@ universe v₁ v₂ v₃ v₄ u₁ u₂ u₃ u₄
 
 variable {C : Type u₁} [Category.{v₁} C] {D : Type u₂} [Category.{v₂} D]
 
-open Opposite
+open Opposite Bifunctor
+open scoped Prod
 
 /-- Dinatural transformations between two (di)functors.
 -/
-structure DinatTrans (F G : Cᵒᵖ ⥤ C ⥤ D) : Type max u₁ v₂ where
+structure DinatTrans (F G : Cᵒᵖ × C ⥤ D) : Type max u₁ v₂ where
   /-- The component of a natural transformation. -/
-  app : ∀ X : C, F.obj₂ (op X) X ⟶ G.obj₂ (op X) X
+  app (X : C) : F.obj ⟨op X, X⟩ ⟶ G.obj ⟨op X, X⟩
   /-- The commutativity square for a given morphism. -/
-  dinaturality :
-    ∀ {X Y : C}
-      (f : X ⟶ Y),
-      (F.map f.op).app X ≫ app X ≫ (G.obj (op X)).map f =
-      (F.obj (op Y)).map f ≫ app Y ≫ (G.map f.op).app Y :=
+  dinaturality {X Y : C} (f : X ⟶ Y) :
+      F.map (f.op ×ₘ 𝟙 _) ≫ app X ≫ G.map (𝟙 (op _) ×ₘ f) =
+      F.map (𝟙 (op _) ×ₘ f) ≫ app Y ≫ G.map (f.op ×ₘ 𝟙 _) :=
         by aesop_cat
 
 attribute [reassoc (attr := simp)] DinatTrans.dinaturality
@@ -46,40 +45,34 @@ attribute [reassoc (attr := simp)] DinatTrans.dinaturality
 /-- Notation for dinatural transformations. -/
 infixr:50 " ⤞ " => DinatTrans
 
-/-- Opposite of a difunctor.
--/
+/-- Opposite of a difunctor. -/
 @[simps!]
-def Functor.diop (F : Cᵒᵖ ⥤ C ⥤ D) : Cᵒᵖ ⥤ C ⥤ Dᵒᵖ := F.biop.flip
+def Functor.diop (F : Cᵒᵖ × C ⥤ D) : Cᵒᵖ × C ⥤ Dᵒᵖ := Bifunctor.flip <| Bifunctor.biop F
 
 variable {E : Type u₃} [Category.{v₃} E] {F : Type u₄} [Category.{v₄} F]
 
-variable {F G H : Cᵒᵖ ⥤ C ⥤ D}
+variable {F G H : Cᵒᵖ × C ⥤ D}
 
-/-- Post-composition with a natural transformation.
--/
+/-- Post-composition with a natural transformation. -/
 @[simps]
 def DinatTrans.compNatTrans (δ : F ⤞ G) (α : G ⟶ H) : F ⤞ H
     where
-  app X := δ.app X ≫ α.app₂ (op X) X
-  dinaturality f := by
-    rw [Category.assoc, Category.assoc, ← NatTrans.naturality_app,
-      ← δ.dinaturality_assoc f, NatTrans.naturality]
+  app X := δ.app X ≫ α.app (op X, X)
+  dinaturality f := by simp; rw [←α.naturality, reassoc_of% δ.dinaturality f,←α.naturality]
 
-/-- Pre-composition with a natural transformation.
--/
+/-- Pre-composition with a natural transformation. -/
 @[simps]
 def DinatTrans.precompNatTrans (δ : G ⤞ H) (α : F ⟶ G) : F ⤞ H
     where
-  app X := α.app₂ (op X) X ≫ δ.app X
-  dinaturality {X Y} f := by
-    rw [Category.assoc, Category.assoc, NatTrans.naturality_assoc,
-      ← δ.dinaturality, NatTrans.naturality_app_assoc]
+  app X := α.app (op X, X) ≫ δ.app X
+  dinaturality {X Y} f := by simp only [Category.assoc, NatTrans.naturality_assoc, dinaturality]
 
-/-- Opposite of a dinatural transformation.
--/
+/-- Opposite of a dinatural transformation. -/
 @[simps]
 def DinatTrans.op (α : F ⤞ G) : G.diop ⤞ F.diop
     where
-  app X := (α.app X).op
+  app X := by simp; exact (α.app X).op
   dinaturality f := Quiver.Hom.unop_inj <| by
     convert α.dinaturality f using 1 <;> exact Category.assoc _ _ _
+
+end CategoryTheory
--- Mathlib/CategoryTheory/Products/Bifunctor.lean
+++ Mathlib/CategoryTheory/Products/Bifunctor.lean
@@ -1,23 +1,16 @@
 /-
 Copyright (c) 2017 Kim Morrison. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Stephen Morgan, Scott Morrison, Andrea Laretto
+Authors: Stephen Morgan, Kim Morrison
 -/
 import Mathlib.CategoryTheory.Products.Basic
-import Mathlib.Tactic.CategoryTheory.Reassoc
-import Mathlib.CategoryTheory.Opposites
 
 /-!
 # Lemmas about functors out of product categories.
 -/
 
-open CategoryTheory
 
-universe v₁ v₂ v₃ v₄ u₁ u₂ u₃ u₄
-
-variable {C : Type u₁} {D : Type u₂} {E : Type u₃} {F : Type u₄}
-
-variable [Category.{v₁} C] [Category.{v₂} D] [Category.{v₃} E] [Category.{v₄} F]
+open CategoryTheory Opposite Prod
 
 namespace CategoryTheory.Bifunctor
 
@@ -25,8 +18,6 @@ universe v₁ v₂ v₃ u₁ u₂ u₃
 
 variable {C : Type u₁} {D : Type u₂} {E : Type u₃}
 variable [Category.{v₁} C] [Category.{v₂} D] [Category.{v₃} E]
-
-open scoped Prod
 
 @[simp]
 theorem map_id (F : C × D ⥤ E) (X : C) (Y : D) :
@@ -53,56 +44,14 @@ theorem diagonal' (F : C × D ⥤ E) (X X' : C) (f : X ⟶ X') (Y Y' : D) (g : Y
     F.map (f ×ₘ 𝟙 Y) ≫ F.map (𝟙 X' ×ₘ g) = F.map (f ×ₘ g) := by
   rw [← Functor.map_comp, prod_comp, Category.id_comp, Category.comp_id]
 
-end CategoryTheory.Bifunctor
-
-namespace CategoryTheory.Functor
-
-/-- Opposite of a bifunctor.
--/
+/-- Opposite of a bifunctor -/
 @[simps!]
-def biop (F : Cᵒᵖ ⥤ D ⥤ E) : C ⥤ Dᵒᵖ ⥤ Eᵒᵖ := F.rightOp ⋙ Functor.opHom _ _
+def biop (F : C × D ⥤ E) : Cᵒᵖ × Dᵒᵖ ⥤ Eᵒᵖ :=
+  (prodOpEquiv C).inverse ⋙ F.op
 
-end CategoryTheory.Functor
+/-- Flipping of arguments of a bifunctor -/
+@[simps!]
+def flip (F : C × D ⥤ E) : D × C ⥤ E :=
+  swap D C ⋙ F
 
-namespace CategoryTheory
-
-variable {C₁ C₂ : C} {D₁ D₂ : D} {E₁ E₂ : E}
-
-/-- Action of two-variable functors on objects. -/
-abbrev Functor.obj₂ (H : C ⥤ D ⥤ E) (A : C) (B : D) : E := (H.obj A).obj B
-
-/-- Action of three-variable functors on objects. -/
-abbrev Functor.obj₃ (H : C ⥤ D ⥤ E ⥤ F) (A : C) (B : D) (C : E) : F :=
-  ((H.obj A).obj B).obj C
-
-/-- Apply a natural transformation between bifunctors to two objects. -/
-abbrev NatTrans.app₂ {F G : C ⥤ D ⥤ E} (α : NatTrans F G) (X : C) (Y : D) :
-    F.obj₂ X Y ⟶ G.obj₂ X Y :=
-  (α.app X).app Y
-
-/-- Apply a natural transformation between bifunctors in three variables to three objects. -/
-abbrev NatTrans.app₃ {H G : C ⥤ D ⥤ E ⥤ F} (α : NatTrans H G) (X : C) (Y : D) (Z : E) :
-    H.obj₃ X Y Z ⟶ G.obj₃ X Y Z :=
-  ((α.app X).app Y).app Z
-
-/- Natural transformations between functors with many variables. -/
-namespace NatTrans
-
-@[reassoc, simp]
-lemma comp_app₂ {H G K : C ⥤ D ⥤ E} (α : H ⟶ G) (β : G ⟶ K) (X : C) (Y : D) :
-    (α ≫ β).app₂ X Y = α.app₂ X Y ≫ β.app₂ X Y := rfl
-
-@[reassoc, simp]
-lemma comp_app₃ {H G K : C ⥤ D ⥤ E ⥤ F} (α : H ⟶ G) (β : G ⟶ K) (X : C) (Y : D)
-    (Z : E) : (α ≫ β).app₃ X Y Z = α.app₃ X Y Z ≫ β.app₃ X Y Z := rfl
-
-@[reassoc (attr := simp)]
-lemma naturality_app_app
-    {G H : C ⥤ D ⥤ E ⥤ F} (T : NatTrans G H) (Y : D) (Z : E) {X X' : C} (f : X ⟶ X') :
-    ((G.map f).app Y).app Z ≫ ((T.app X').app Y).app Z =
-      ((T.app X).app Y).app Z ≫ ((H.map f).app Y).app Z :=
-  congr_app (NatTrans.naturality_app T Y f) Z
-
-end NatTrans
-
-end CategoryTheory
+end CategoryTheory.Bifunctor
